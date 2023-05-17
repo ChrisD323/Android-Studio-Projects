7d032cac0e1634a6ee321cda704b26b5e4dd6d8d
ChrisD323 <drikosx323@gmail.com>
2023-05-17 16:37:16 +0300

    Dialejh 10 Xampp

---
+++ app/src/main/java/com/example/carpicker/Media.java
@@ -0,0 +1,11 @@
+package com.example.carpicker;
+
+class Media{
+    private String image;
+    public Media(String image) {
+        this.image = image;
+    }
+    public String getImage() {
+        return image;
+    }
+}
